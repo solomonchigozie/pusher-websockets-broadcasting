4683592426755499f9f702fa0105ebd76f47ce3a
solomon chigozie <solomon1chigozie@gmail.com>
2024-10-18 20:04:57 +0100

    private channel authentication added

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Broadcast</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <h1>Messages</h1>
+    <div id="messages"></div>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Events\NewMessageEvent;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/messages', function(){
+    return view('messages');
+});
+
+Route::get('/send',  function() {
+    event(new NewMessageEvent('hello this is me'));
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
